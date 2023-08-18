5f6e6f564c8b68fdc8af3c10ec865d12dcabdff0
Noah <noah.ntabo@student.moringaschool.com>
2023-08-18 20:52:50 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evenList = [n for n in num_list if n % 2 == 0]
+    return evenList
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamationlist=[sentence + "!" for sentence in sentence_list] 
+
+    return exclamationlist
